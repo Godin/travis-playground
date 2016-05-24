878c5fc83be387aeaebb1129face1c26103df120
Evgeny Mandrikov <mandrikov@gmail.com>
2016-05-24 16:17:30 +0200

    libssl

--- travis.sh
+++ travis.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 set -euo pipefail
-
-curl -v -LsS http://www.java.net/download/java/jdk8u102/archive/b04/binaries/jdk-8u102-ea-bin-b04-linux-x64-25_apr_2016.tar.gz -o jdk.tar.gz
-
-tar -xzf jdk.tar.gz -C /tmp/jdk --strip-components 1
